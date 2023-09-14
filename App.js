0a08c3f356aceb3acb800f7e49a5985a0d5a477c
Javier Martín Gómez <j.martingomez@edu.gva.es>
2023-09-14 13:51:12 +0200

    Co-authored-by: nazarbk <nazarbk@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Card style={styles.card}>
+      <Card.Content>
+        <Icon name="home" size={40} color="#3F51B5" />
+        <Title>Opción 1</Title>
+        <Paragraph>Descripción de la opción 1</Paragraph>
+      </Card.Content>
+    </Card>
+
+    <Card style={styles.card}>
+      <Card.Content>
+        <Icon name="star" size={40} color="#FFC107" />
+        <Title>Opción 2</Title>
+        <Paragraph>Descripción de la opción 2</Paragraph>
+      </Card.Content>
+    </Card>
+
+    <Card style={styles.card}>
+      <Card.Content>
+        <Icon name="calendar" size={40} color="#4CAF50" />
+        <Title>Opción 3</Title>
+        <Paragraph>Descripción de la opción 3</Paragraph>
+      </Card.Content>
+    </Card>
+
+    <Card style={styles.card}>
+      <Card.Content>
+        <Icon name="settings" size={40} color="#FF5722" />
+        <Title>Opción 4</Title>
+        <Paragraph>Descripción de la opción 4</Paragraph>
+      </Card.Content>
+    </Card>
+
+    <Card style={styles.card}>
+      <Card.Content>
+        <Icon name="help-circle" size={40} color="#9E9E9E" />
+        <Title>Opción 5</Title>
+        <Paragraph>Descripción de la opción 5</Paragraph>
+      </Card.Content>
+    </Card>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: '80%',
+    marginVertical: 10,
+    padding: 10,
   },
 });
